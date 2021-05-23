8caf72bcfb2f3ff018e3c8f4f6d3f0399cbd28b8
Eric <eric56379@gmail.com>
2021-05-23 13:42:08 -0500

    Added background slideshow and new images.

--- PREEVENTS/home-script.js
+++ PREEVENTS/home-script.js
@@ -1,10 +1,15 @@
-var imageURLs = ['https://github.com/UtahEFD/UtahEFD.github.io/blob/main/PREEVENTS/Images/University-of-MN-Duluth-Campus-min-min.jpg?raw=true',
-'https://github.com/UtahEFD/UtahEFD.github.io/blob/main/PREEVENTS/Images/University-of-Utah-Campus-min-min.jpg?raw=true'];
-
-var index = 0;
+function cycleBackgroundImages() {
+  $.backstretch([
+    "https://github.com/UtahEFD/UtahEFD.github.io/blob/main/PREEVENTS/Images/University-of-MN-Duluth-Campus-min-min.jpg?raw=true",
+    "https://github.com/UtahEFD/UtahEFD.github.io/blob/main/PREEVENTS/Images/University-of-Utah-Campus-min-min.jpg?raw=true",
+    ], {
+      fade: 750,
+      duration: 3000
+  });
+}
 
 $(document).ready(function() {
-  backgroundSlides();
+  cycleBackgroundImages();
 
   // Fades the intro text in.
   $(".container, #preeventsTitle").animate({opacity: 1}, 1500);
@@ -17,16 +22,5 @@ $(document).ready(function() {
   $(".container, .buttonsRowOne, .publicationsButton").delay(2700).animate({opacity: 1}, 200);
   $(".container, .buttonsRowOne, .talksButton").delay(2900).animate({opacity: 1}, 200);
   $(".container, .buttonsRowOne, .visualizationsButton").delay(3100).animate({opacity: 1}, 200);
-});
 
-function backgroundSlides() {
-  if (index >= imageURLs.length) {
-    index = 0;
-  }
-  
-  $("body")
-  .css("background-image", "linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(" + imageURLs[index++] + ")")
-  .fadeIn(1000, function() {
-    setTimeout(backgroundSlides, 4000);
-  });
-}
+});
